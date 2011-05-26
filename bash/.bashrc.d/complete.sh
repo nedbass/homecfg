2317610f2b7c56b5bd548e93a64ef64e2f0129cc
Prakash Surya <prakash.surya56@gmail.com>
2011-05-26 09:48:00 -0700

    Added complete.sh

---
+++ bash/.bashrc.d/complete.sh
@@ -0,0 +1,12 @@
+##########################
+# Start complete Exports #
+##########################
+
+# It is often useful to have the auto-complete feature (pressing Tab key twice
+# on the keyboard) after you type certain commands (like sudo or man).
+complete -cf sudo
+complete -cf man
+
+########################
+# End complete Exports #
+########################
