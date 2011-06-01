016fb68002ad06ee36c747f6133e03ebab843e46
Prakash Surya <prakash.surya56@gmail.com>
2011-06-01 08:35:12 -0700

    Added $PS1 customization to bash exports script

--- bash/.bashrc.d/50_export.sh
+++ bash/.bashrc.d/50_export.sh
@@ -2,6 +2,16 @@
 # Start Environment Exports #
 #############################
 
+# Customize by bash interactive shell prompt
+# * \H : Hostname
+# * \u : Username of current user
+# * \d : Date in "Weekday Month Date" format (e.g. "Tue May 26")
+# * \t : Current Time in 24-hour notation (HH:MM:SS)
+# * \w : Current working directory, with $HOME abbreviated with ~
+# * \n : Newline
+# * \$ : If UID is 0, print a '#' character, otherwise print a '$' character
+export PS1="$Yellow\H \u \d \t \w\n\$ $Color_Off"
+
 export GREP_COLOR='1;33' # Set the color to use when highlighting the matched
                          # part of grep's output (1;33 ~> yellow)
 export LESS='-R'
