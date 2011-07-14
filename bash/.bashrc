cc98f50c8dc6dd34f1efda8890f8fec898f6c0ef
Prakash Surya <surya1@llnl.gov>
2011-07-14 14:10:11 -0700

    Add git-completion.bash and update bash configs
     * Added the git-completion.bash file as 30_git-completion.bash
     * The bashrc config file to correctly source this new file extension (*.bash)
     * Modified the $PS1 customization to make use of the new __git_ps1 function
       git-completion.bash provides. Thus, if in a git branch, the branch name will
       be shown in the $PS1 prompt.

--- bash/.bashrc
+++ bash/.bashrc
@@ -12,13 +12,14 @@ set -o vi
 PATH="$PATH:$HOME/bin"
 
 # * Source the bash configuration files in $HOME/.bashrc.d
-# * Files in this directory must have '.sh' extension to be sourced
+# * Files in this directory must have a '.sh' or a '.bash' extension to be
+#   sourced
 # * Files must also be prefixed by a two digit number. This number
 #   specifies the order in which the files are sourced (lower numbered
 #   prefixes being sourced first).
 # * If a file does not conform to the above constraints, the results are
 #   undefined.
-for file in `ls $HOME/.bashrc.d/*.sh | sort -n`; do
+for file in `ls $HOME/.bashrc.d/*.sh $HOME/.bashrc.d/*.bash | sort -n`; do
 	[ -r $file ] && . $file
 done
 unset file
--- bash/.bashrc.d/50_export.sh
+++ bash/.bashrc.d/50_export.sh
@@ -2,15 +2,24 @@
 # Start Environment Exports #
 #############################
 
+export GIT_PS1_SHOWUPSTREAM="verbose" # Used in conjunction with __git_ps1 in
+                                      # the $PS1 customization. Used to show
+                                      # the difference between HEAD and its
+                                      # upstream. "verbose" means, show number
+                                      # of commits ahead/behind (+/-) upstream.
+
 # Customize by bash interactive shell prompt
 # * \H : Hostname
 # * \u : Username of current user
 # * \d : Date in "Weekday Month Date" format (e.g. "Tue May 26")
 # * \t : Current Time in 24-hour notation (HH:MM:SS)
 # * \w : Current working directory, with $HOME abbreviated with ~
+# * $(__git_ps1 " (%s)") : Show the name of the current branch if your are
+#                          currently in a git repository (depends on the
+#                          git-completion.bash file)
 # * \n : Newline
 # * \$ : If UID is 0, print a '#' character, otherwise print a '$' character
-export PS1="$Yellow\H \u \d \t \w\n\$ $Color_Off"
+export PS1="$Yellow\H \u \d \t \w\$(__git_ps1 ' (%s)')\n\$ $Color_Off"
 
 export GREP_COLOR='1;33' # Set the color to use when highlighting the matched
                          # part of grep's output (1;33 ~> yellow)
